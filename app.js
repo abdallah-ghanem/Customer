d9733c9ed1821a77beb75908307329aaf96e062e
Abdallah <72445656+abdallah-ghanem@users.noreply.github.com>
2024-11-24 10:44:18 +0200

    Add My SQL Connections

--- app.js
+++ app.js
@@ -55,6 +55,25 @@ mongoose
         console.log(err);
     });
 //====================================================================================================
+
+    /* var mysql = require('mysql');
+
+    var con = mysql.createConnection({
+        host: "localhost",
+        user: "yourusername",
+        password: "yourpassword",
+        database: "mydb"
+    });
+    
+    con.connect(function(err) {
+        if (err) throw err;
+        var sql = "DELETE FROM customers WHERE address = 'Mountain 21'";
+        con.query(sql, function (err, result) {
+        if (err) throw err;
+        console.log("Number of records deleted: " + result.affectedRows);
+        });
+    }); */
+//====================================================================================================
 //middelware to use routes
 app.use(allRoutes);
 app.use("/user/add.html", addUserRoute);
